484c741f2f80d9d33a810f2e652463cfc4fdb4b5
Manutai Teraiamano <manutai@teraiamano.com>
2022-09-26 18:23:05 +0200

    first commit

---
+++ src/main/java/com/example/sipfapi/SipfApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sipfapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipfApiApplication.class, args);
+	}
+
+}
